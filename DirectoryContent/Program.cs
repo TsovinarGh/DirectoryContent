2b04cb4b1fe31b8c1f4accc73541167cf8f68964
Tsovinar <Tsovinar.ghazaryan@yahoo.com>
2017-02-02 02:40:04 +0400

    DirectoryContent

---
+++ DirectoryContent/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drives = Directory.GetLogicalDrives();
+            
+            PrintDirectoryContents(@"D:\C#2016\Starting\");
+            Console.ReadKey();
+        }
+
+        public static void PrintDirectoryContents(string path, string prefix = "")
+        {
+            string[] directories = null;
+
+            try
+            {
+                directories = Directory.GetDirectories(path);
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                return;
+            }
+
+            var files = Directory.GetFiles(path);
+
+            foreach(var directory in directories)
+            {
+                var fileTokens = directory.Split('\\');
+                Console.WriteLine(prefix + fileTokens.Last());
+                PrintDirectoryContents(directory, prefix + "     ");
+            }
+
+            foreach(var file in files)
+            {
+                var fileTokens = file.Split('\\');
+                Console.WriteLine(prefix + fileTokens.Last());
+            }
+        }
+    }
+}
